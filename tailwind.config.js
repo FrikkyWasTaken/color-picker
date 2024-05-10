a393f68cbb86c0ad494d047becf628eceeb5e915
Joey Jooste <joeyjjooste@gmail.com>
2024-05-10 11:15:03 +0200

    updated tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: { fontFamily: { arial: ["Arial", "sans-serif"] } },
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+        Syncopate: ["Syncopate", "sans-serif"],
+      },
+      colors: {
+        ctaBlue: "#2066DB",
+        textBoxGrey: "#F4F4F4",
+      },
+      fontSize: {
+        bodyFontSize: "16px",
+        smallTextSize: "14px",
+        headingOneSize: "48px",
+        headingTwoSize: "24px",
+        headingThreeSize: "20px",
+      },
+    },
   },
   plugins: [],
 };
